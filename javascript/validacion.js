c160e2b29c1b345dc15dbb44be900f3f1b9cb5e4
Beayemi <emiliopardocantillana@gmail.com>
2024-05-17 00:40:42 -0400

    final 0.1

--- javascript/validacion.js
+++ javascript/validacion.js
@@ -20,6 +20,12 @@ $(document).ready(function () {
             return;
         }
 
+        var phonePattern = /^\d+$/;
+        if (!phonePattern.test(celular)) {
+            alert('Por favor ingresa un número de teléfono válido.');
+            return;
+        }
+
         setTimeout(function () {
             alert('El formulario se ha enviado correctamente.');
             $('#contact-form')[0].reset(); 
